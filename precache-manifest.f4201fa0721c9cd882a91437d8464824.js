9759a1f723c3bd44bb0737f14fee4ae25fd73e5c
Oleg Hansen <oleg-ucb@outlook.com>
2019-01-26 00:04:25 -0800

    Updates

---
+++ precache-manifest.f4201fa0721c9cd882a91437d8464824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d093d11a6420a224d347",
+    "url": "/reactgame/static/js/runtime~main.d093d11a.js"
+  },
+  {
+    "revision": "5248695a9638e3a68029",
+    "url": "/reactgame/static/js/main.5248695a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/reactgame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5248695a9638e3a68029",
+    "url": "/reactgame/static/css/main.449cd89d.chunk.css"
+  },
+  {
+    "revision": "f577175495f877c5c2b1a9fc488f75cd",
+    "url": "/reactgame/index.html"
+  }
+];
